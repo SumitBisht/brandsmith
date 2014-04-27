9fe71c76a9b8a6dd0017c7d388559ef088c67091
Sumit Bisht <sumitbisht1987@gmail.com>
2014-04-27 22:32:22 +0530

    added basic site pages

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 
-var routes = require('./routes');
-var express = require('express');
-var http = require('http');
-var path = require('path');
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    http = require('http'),
+    path = require('path'),
+    app = express();
 
-var http = require('http');
-var path = require('path');
-var app = express();
-
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.logger('dev'));
@@ -16,11 +14,28 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.bodyParser());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/', routes.index);
+app.get('/', function(req, res){
+	res.render('index', { title: 'Welcome',
+	  images: ['front.jpg', 'front1.jpg', 'front2.jpg'] 
+	});
+});
+
+app.get('/about', function(req, res){
+	res.render('about_us', { 
+  	title: 'About Us', 
+  	address: '123 somewhere street', 
+  	contact: '12345', 
+  	email:'user@email.com'
+  });
+});
+
+app.get('/*', function(req, res){
+  res.render('404', { title: 'Page Not found', fallbackpage: '/' });
+});
+
 
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
