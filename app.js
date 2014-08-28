b31b297d7ae5d6c4058650a307d6016342fb57f5
sumitbisht <sumitbisht1987@gmail.com>
2014-08-28 08:36:50 +0000

    removed unused server variable

--- app.js
+++ app.js
@@ -55,8 +55,7 @@ app.get('/*', function(req, res){
   res.render('404', { title: 'Page Not found', fallbackpage: '/' });
 });
 
-
-var server = http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
